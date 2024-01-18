705142e678adb7679a9e06cabf80357bec63decd
Sasen Perera <sasen.learnings@gmail.com>
2024-01-18 20:11:23 +0530

    Portfolio : v1.4 : Finalized Portfolio, and added an alert to the Resume Button; Since the Resume is still not Ready

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from random import randint
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_key'] = f'{os.urandom(randint(20, 50))}'
 
-@app.route('/')
+uri = f"mongodb+srv://sas2k:{os.getenv('DBPass')}@clientbase.pk701wj.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
+clients = []
+
+db = client.Clients
+cluster = db.Data
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        data = request.form
+        items = {
+            'Name': data['UserName'],
+            'Email': data['UserEmail'],
+            'Message': data['TextBody']
+        }
+        cluster.insert_one(items)
+        print(cluster.find_one())
     return render_template('index.html')
 
-@app.route('/projects')
+@app.route('/projects', methods=['GET', 'POST'])
 def projects():
     return render_template('projects.html')
 
--- static/script.js
+++ static/script.js
@@ -1,3 +1,15 @@
+let year = new Date().getFullYear();
+let yearComponent = document.getElementById('year');
+
+yearComponent.innerText = year;
+
+if (window.location && window.location.pathname == '/') {
+    let ageComponent = document.getElementById('age');
+    ageComponent.innerHTML = year - 2008
+}
+
+//Code Below are for Bulma.
+
 document.addEventListener('DOMContentLoaded', () => {
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   
@@ -13,9 +25,45 @@ document.addEventListener('DOMContentLoaded', () => {
   
 });
 
-let year = new Date().getFullYear();
-let ageComponent = document.getElementById('age');
-let yearComponent = document.getElementById('year');
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
 
-ageComponent.innerText = year - 2008;
-yearComponent.innerText = year;
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        closeAllModals();
+    }
+    });
+});
